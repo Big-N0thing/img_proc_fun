37013e4e18ea957c95b5a289cd920306e800343b
Big Nothing <Noefuente12@hotmail.com>
2022-11-30 15:11:02 +0100

    cositas nuevas

--- Microservicio Gestor/src/main/java/org/csvmanager/csvWriter.java
+++ Microservicio Gestor/src/main/java/org/csvmanager/csvWriter.java
@@ -1,8 +1,23 @@
 package org.csvmanager;
 
-import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import com.opencsv.*;
 
 public class csvWriter {
+    public static void main(String[] args) {
+
+        String[] entries = {"ESTO ES LO QUE SE METE AL CSV"};
+
+        try {
+            CSVWriter writer = new CSVWriter(new FileWriter("./pruebaEscritura.csv"));
+
+            writer.writeNext(entries);
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
