08fec8a08146029746f381239e7eedc871f8162d
AntoLiria <afrodriguezl@indra.es>
2022-11-30 13:13:25 +0100

    csvReader class updated with line by line reading

---
+++ Microservicio Gestor/src/main/java/org/csvmanager/csvReader.java
@@ -0,0 +1,37 @@
+package org.csvmanager;
+
+import java.io.FileReader;
+/*import java.nio.file.Path;
+import java.nio.file.Paths;*/
+
+import com.opencsv.*;
+
+public class csvReader
+{
+	private static String CSV_FILE_PATH = "C:/Users/afrodriguezl/Desktop/example.csv";	
+
+    public static void main( String[] args )
+    {    	
+    	readDataLineByLine(CSV_FILE_PATH);
+    }  
+    
+	 public static void readDataLineByLine(String file)
+	 {	   
+	     try {	   
+	         FileReader filereader = new FileReader(file);
+	         CSVReader CsvReader = new CSVReader(filereader);
+	         String[] nextRecord;
+	   
+	     while ((nextRecord = CsvReader.readNext()) != null) {
+	         for (String cell : nextRecord) {
+	             System.out.print(cell + "\t");
+	             }
+	             System.out.println();
+	         }
+	     }
+	     catch (Exception e) {
+	         e.printStackTrace();
+	     }
+	 }
+    
+}
