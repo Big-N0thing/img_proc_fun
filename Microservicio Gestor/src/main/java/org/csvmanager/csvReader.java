443e53b2a2ea7dd6d165a768ea5e4e3e1c3f24db
Big Nothing <Noefuente12@hotmail.com>
2022-11-30 13:22:58 +0100

    refactor rápido

--- Microservicio Gestor/src/main/java/org/csvmanager/csvReader.java
+++ Microservicio Gestor/src/main/java/org/csvmanager/csvReader.java
@@ -6,32 +6,28 @@ import java.nio.file.Paths;*/
 
 import com.opencsv.*;
 
-public class csvReader
-{
-	private static String CSV_FILE_PATH = "./pruebaLectura.csv";
+public class csvReader {
+    private static String CSV_FILE_PATH = "./pruebaLectura.csv";
 
-    public static void main( String[] args )
-    {    	
-    	readDataLineByLine(CSV_FILE_PATH);
-    }  
-    
-	 public static void readDataLineByLine(String file)
-	 {	   
-	     try {	   
-	         FileReader filereader = new FileReader(file);
-	         CSVReader CsvReader = new CSVReader(filereader);
-	         String[] nextRecord;
-	   
-	     while ((nextRecord = CsvReader.readNext()) != null) {
-	         for (String cell : nextRecord) {
-	             System.out.print(cell + "\t");
-	             }
-	             System.out.println();
-	         }
-	     }
-	     catch (Exception e) {
-	         e.printStackTrace();
-	     }
-	 }
-    
+    public static void main(String[] args) {
+        readDataLineByLine(CSV_FILE_PATH);
+    }
+
+    public static void readDataLineByLine(String file) {
+        try {
+            FileReader filereader = new FileReader(file);
+            CSVReader CsvReader = new CSVReader(filereader);
+            String[] nextRecord;
+
+            while ((nextRecord = CsvReader.readNext()) != null) {
+                for (String cell : nextRecord) {
+                    System.out.print(cell + "\t");
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
---
+++ Microservicio Gestor/src/main/java/org/csvmanager/csvWriter.java
@@ -0,0 +1,8 @@
+package org.csvmanager;
+
+import java.io.FileReader;
+
+import com.opencsv.*;
+
+public class csvWriter {
+}
