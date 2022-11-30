92d81c403b6d04b7d7b4f022aed6beb2e1cc0c3c
AntoLiria <afrodriguezl@indra.es>
2022-11-30 15:59:01 +0100

    Función para leer todo el contenido del csv  junto
    Función readAllData(String file)

--- Microservicio Gestor/src/main/java/org/csvmanager/csvReader.java
+++ Microservicio Gestor/src/main/java/org/csvmanager/csvReader.java
@@ -1,33 +1,55 @@
 package org.csvmanager;
 
 import java.io.FileReader;
-/*import java.nio.file.Path;
-import java.nio.file.Paths;*/
+import java.io.IOException;
+import java.util.List;
 
 import com.opencsv.*;
 
 public class csvReader {
-    private static String CSV_FILE_PATH = "./pruebaLectura.csv";
+    private static String CSV_FILE_PATH = "./pruebaEscritura.csv";
 
     public static void main(String[] args) {
-        readDataLineByLine(CSV_FILE_PATH);
+    	System.out.println("READ DATA LINE BY LINE\n");
+    	readDataLineByLine(CSV_FILE_PATH);
+    	System.out.println("_______________________________________________");
+    	System.out.println("READ ALL DATA\n");
+        readAllData(CSV_FILE_PATH);
+        System.out.println("_______________________________________________");
     }
 
     public static void readDataLineByLine(String file) {
         try {
             FileReader filereader = new FileReader(file);
-            CSVReader CsvReader = new CSVReader(filereader);
+            CSVReader reader = new CSVReader(filereader);
             String[] nextRecord;
 
-            while ((nextRecord = CsvReader.readNext()) != null) {
+            while ((nextRecord = reader.readNext()) != null) {
                 for (String cell : nextRecord) {
                     System.out.print(cell + "\t");
                 }
                 System.out.println();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            reader.close();
+        }catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
-
+    
+    public static void readAllData(String file) {
+        try {
+            FileReader filereader = new FileReader(file);
+            CSVReader reader = new CSVReaderBuilder(filereader).build();
+            List<String[]> allData = reader.readAll();
+      
+            for (String[] row : allData) {
+                for (String cell : row) {
+                    System.out.print(cell + "\t");
+                }
+                System.out.println();
+            }
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
